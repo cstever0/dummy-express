c0eb89070e3291d7e5e7b58116b6de8b8a17e8a7
Cory Stever <cstever@hotmail.com>
2024-03-26 10:02:44 -0500

    Still Works

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-// const express = require("express");
+const express = require("express");
 
-// const app = express();
+const app = express();
 
-// require('dotenv').config();
+require('dotenv').config();
 
-// app.use(express.json());
+app.use(express.json());
 
-// // 1:
-// app.get('/', (req, res, next) => {
-//     console.log('number 1 was triggered')
-//     next(err);
-//   });
+// 1:
+app.get('/', (req, res, next) => {
+    console.log('number 1 was triggered')
+    next(err);
+  });
 
-//   // 2:
-//   app.use((err, req, res, next) => {
-//     console.log('number 2 was triggered')
-//     next(err);
-//   });
+  // 2:
+  app.use((err, req, res, next) => {
+    console.log('number 2 was triggered')
+    next(err);
+  });
 
-//   // 3:
-//   app.use((err, req, res, next) => {
-//     console.log('number 3 was triggered')
-//     return res.json();
-//   });
+  // 3:
+  app.use((err, req, res, next) => {
+    console.log('number 3 was triggered')
+    return res.json();
+  });
 
-//   // 4:
-//   app.use((req, res, next) => {
-//     console.log('number 4 was triggered')
-//     next();
-//   });
+  // 4:
+  app.use((req, res, next) => {
+    console.log('number 4 was triggered')
+    next();
+  });
 
-// const PORT = process.env.PORT;
-// app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
